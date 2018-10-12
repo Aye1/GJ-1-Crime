8615cbc57087e963a8c86b218d2afef2ad3a6849
D1m1t40v <dimitri.quesnel@gmail.com>
2018-10-13 01:39:39 +0200

    Add mock inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private static Inventory _instance;
+    public static Inventory Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if(_instance !=null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
