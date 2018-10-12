e3a1771eda0ba95591e8f777e3891d468c072c41
D1m1t40v <dimitri.quesnel@gmail.com>
2018-10-12 22:53:05 +0200

    First implem of player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    List<IInteractable> currentInteractablesList = new List<IInteractable>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        IInteractable otherObj = collision.gameObject.GetComponent<IInteractable>();
+        if (otherObj != null)
+        {
+            currentInteractablesList.Add(otherObj);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        IInteractable otherObj = collision.gameObject.GetComponent<IInteractable>();
+        if (otherObj != null)
+        {
+            currentInteractablesList.Remove(otherObj);
+        }
+    }
+}
