9a2d0fe52c6b6aa3311e6f011f4295545b47d3b7
D1m1t40v <dimitri.quesnel@gmail.com>
2018-10-13 03:23:38 +0200

    .Net 4.0 + camera scripting

--- Assets/Scripts/DancefloorTile.cs
+++ Assets/Scripts/DancefloorTile.cs
@@ -1,43 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Random = System.Random;
 
-public class DancefloorTile : MonoBehaviour {
-
+public class DancefloorTile : MonoBehaviour
+{
     [Header("Readonly")]
     public SpriteRenderer haloLight;
     public bool isLightOn = false;
     private Random random;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         random = new Random();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
+    }
 
-    public void ManageLight(Color color) {
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void ManageLight(Color color)
+    {
         SwitchLight(random.NextDouble() <= 0.5f, color);
     }
 
-    private void SwitchLight(bool on, Color color) {
-        if(on) {
+    private void SwitchLight(bool on, Color color)
+    {
+        if (on)
+        {
             LightOn(color);
-        } else {
+        }
+        else
+        {
             LightOff();
         }
     }
 
-    private void LightOn(Color color) {
+    private void LightOn(Color color)
+    {
         haloLight.gameObject.SetActive(true);
         haloLight.color = color;
         GetComponent<SpriteRenderer>().color = color;
     }
 
-    private void LightOff() {
+    private void LightOff()
+    {
         //haloLight.gameObject.SetActive(false);
     }
 }
