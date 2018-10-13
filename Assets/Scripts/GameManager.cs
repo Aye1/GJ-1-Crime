34a16e9a40c3613f3bf5a5632f5e646c73268bf2
Adrien Janvier <janvier.adrien@gmail.com>
2018-10-13 14:59:55 +0200

    Empty GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager _instance;
+
+    public static GameManager Instance {
+        get { return _instance; }
+    }
+
+	// Use this for initialization
+	void Start () {
+        if(_instance != null && _instance != this) {
+            Destroy(gameObject);
+        } else 
+        {
+            _instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void FreezeAllButText() {
+
+    }
+}
