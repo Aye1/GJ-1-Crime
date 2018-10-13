d20f7284ac7ea7c6e83f1145293247b487c19c56
Adrien Janvier <janvier.adrien@gmail.com>
2018-10-13 10:22:16 +0200

    Dialog box

---
+++ Assets/Freddy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Freddy : Character {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected override DialogLine[] GetDialogLines()
+    {
+        DialogLine[] lines = {
+            new DialogLine("Line 1"),
+            new DialogLine("Line 2")
+        };
+        return lines;
+    }
+}
