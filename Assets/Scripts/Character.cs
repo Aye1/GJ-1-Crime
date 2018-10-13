5d9c087aeef74fcf0045ef74b28476dedb73a512
Adrien Janvier <janvier.adrien@gmail.com>
2018-10-13 09:43:53 +0200

    Imperial march + import DialogBox

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class Character : MonoBehaviour, IInteractable {
+
+    public Sprite picture;
+    public string characterName;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Interact()
+    {
+        DialogManager.Instance.StartDialog(GetDialogLines());
+    }
+
+    protected abstract DialogLine[] GetDialogLines();
+}
