92b1839a94731af45b80e80ac47d8e3aee5b3edc
Adrien Janvier <janvier.adrien@gmail.com>
2018-10-13 17:03:32 +0200

    Hall music + better music blending

--- Assets/Scripts/AudioStopAtDistance.cs
+++ Assets/Scripts/AudioStopAtDistance.cs
@@ -7,20 +7,17 @@ public class AudioStopAtDistance : MonoBehaviour {
 
     Player _player;
     AudioSource _source;
-    public float maxDist = 25.0f;
+    public float maxDist;
 
     // Use this for initialization
     void Start () {
         _player = FindObjectOfType<Player>();
-        _source = FindObjectOfType<AudioSource>();
+        _source = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if(Vector3.Distance(_player.transform.position, gameObject.transform.position) > maxDist) {
-            _source.volume = 0.0f;
-        } else {
-            _source.volume = 1.0f;
-        }
+        float dist = Vector3.Distance(_player.transform.position, gameObject.transform.position);
+        _source.mute = dist > maxDist;
 	}
 }
