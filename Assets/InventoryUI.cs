5a67267ac25518e2e7ef04cccd342ce728892a5d
Adrien Janvier <janvier.adrien@gmail.com>
2018-10-14 12:30:16 +0200

    Basic inventory

---
+++ Assets/InventoryUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryUI : MonoBehaviour {
+
+    [Header("UI Bindings")]
+    public Image scrollImage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ManageVisibility();
+	}
+
+    private void ManageVisibility() {
+        scrollImage.gameObject.SetActive(Inventory.Instance.HasFreddyScroll);
+    }
+}
