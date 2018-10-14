35c7b670c531fb4619b5fd98f2da7bac331357a4
Adrien Janvier <janvier.adrien@gmail.com>
2018-10-14 13:31:08 +0200

    Canvas for inventory

---
+++ Assets/MainCanvas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCanvas : MonoBehaviour {
+
+    private static MainCanvas _instance;
+    public static MainCanvas Instance {
+        get { return _instance; }
+    }
+
+	// Use this for initialization
+	void Start () {
+        if (_instance != this && _instance != null) {
+            Destroy(gameObject);
+        } else {
+            _instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
