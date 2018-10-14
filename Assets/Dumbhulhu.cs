d5a8a8b5ead8e358815c5421e0402de516feb76c
Adrien Janvier <janvier.adrien@gmail.com>
2018-10-14 11:13:40 +0200

    Basic Dumbhulhu

---
+++ Assets/Dumbhulhu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dumbhulhu : Character {
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected override DialogLine[] GetDialogLines()
+    {
+        DialogLine[] lines = {
+            new DialogLine("Bienvenue à la Crime Academy", this)
+        };
+        return lines;
+    }
+
+}
